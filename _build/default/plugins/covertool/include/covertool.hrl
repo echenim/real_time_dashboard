817700de3c1ecf65c8d9e1ea74e456d92304a491
echenim <myron.echenim@gmail.com>
2025-07-17 15:30:01 -0700

    update

--- _build/default/plugins/covertool/include/covertool.hrl
+++
@@ -1,7 +0,0 @@
--record(config, {appname = 'Application',
-                 prefix_len = 0,
-                 cover_data = no_import,
-                 output = "coverage.xml",
-                 summary = false,
-                 sources = ["src/"],
-                 beams = ["ebin/"]}).
